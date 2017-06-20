db813ca8b77c84a556649614701b49f7d36c0e2e
danfujita <dfujita@gmu.edu>
2017-06-20 08:16:58 -0400

    Export articles into json files

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import configparser
 import requests
 from rss.load_articles import load_articles
 from converter import convert
+import json
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -12,8 +13,15 @@ def loader():
     apple_news_url = config['URLS']['apple_news_url']
     syndication_url = config['URLS']['syndication_url']
     articles = load_articles(config['URLS']['syndication_url'],config['AUTH']['username'],config['AUTH']['password'])
+    count = 0
     for article in articles:
         apple_news_article = convert(article)
+        path = 'articles/article'+str(count)+'.json'
+        print(path)
+        apple_news_article= json.loads(apple_news_article)
+        with open(path, 'w+') as outfile:
+             json.dump(apple_news_article, outfile, indent=4)
+        count=count+1
         '''
         upload_article_url = apple_news_url + '/channels/'+channel_id+ '/articles'
         r = requests.put(upload_article_url, json=article)
@@ -21,6 +29,9 @@ def loader():
 
 
 if __name__ == '__main__':
+    loader()
+    '''
     sched = BlockingScheduler(timezone='EST')
     sched.add_job(loader, 'interval', id='loeader',minutes=int(config['INTERVAL']['minutes']))
     sched.start()
+    '''
